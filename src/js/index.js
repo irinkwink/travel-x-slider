1ba485dfb14d8bafd8705f4f3fb99a107c249b6a
irinkwink <irina-ns@yandex.ru>
2023-09-03 22:24:51 +0300

    feat: add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,33 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+
+  loop: true,
+  parallax: true,
+  speed: 1000,
+
+  keyboard: {
+    enabled: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.slider-controls__count',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
